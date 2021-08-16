ba6648ecda893c2131187c130dc857473f9746ac
sansh <sansh@Anshul>
2021-08-16 18:05:47 +0530

    First commit.

---
+++ sampleapp/src/main/java/sampleapp/mainclass.java
@@ -0,0 +1,14 @@
+package sampleapp;
+
+public class mainclass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		System.out.println("How r u");
+		System.out.println("Nice to meet u");
+		// TODO Auto-generated method stub
+
+	}
+
+}
