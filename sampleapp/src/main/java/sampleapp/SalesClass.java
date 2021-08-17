8e8e0a0e6c66fdb7757b1722f0f8248d40d111dc
Anshul <sansh786.as@gmail.com>
2021-08-17 10:51:20 +0530

    Display method added with sum

--- sampleapp/src/main/java/sampleapp/SalesClass.java
+++ sampleapp/src/main/java/sampleapp/SalesClass.java
@@ -5,11 +5,16 @@ public class SalesClass {
 	public void display() {
 		
 		int[] arr= {1,2,3,4,5};
+		int sum=0;
 		for( int num:arr)
 		{
+			sum+=num;
 			System.out.print(num);
 		}
-		
+		System.out.println("Summation: "+ sum);
 	}
-
+	public void print() {
+		System.out.println("print all array numbers: ");
+		display();
+	}
 }
